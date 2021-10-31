163f64e149868ae0e2aa33a1528228bea1cd727e
MissCarolin <carolin.wagner.hh@gmx.de>
2021-10-31 16:56:25 +0100

    styled projects page

--- src/app/views/projects/projects.module.ts
+++ src/app/views/projects/projects.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectsComponent } from './projects.component';
 import { AppProjectsRoutingModule } from './projects.routing.module';
+import { AppHeaderModule } from 'src/app/shared/header/header.module';
 
 
 
 @NgModule({
   declarations: [ProjectsComponent],
   exports: [ProjectsComponent],
-  imports: [CommonModule, AppProjectsRoutingModule]
+  imports: [AppHeaderModule, CommonModule, AppProjectsRoutingModule]
 })
 export class AppProjectsModule { }
